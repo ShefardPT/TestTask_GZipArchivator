1e9fc963907287c9e3d0d71614d706660d65bdf9
Shefer Alexander <shefard55r@gmail.com>
2019-09-09 03:19:25 +0600

    - tests for methods to compress and decompress

---
+++ tests/TestTask_GZipArchiver.Tests/ArchivationServiceTests.cs
@@ -0,0 +1,61 @@
+using System.IO;
+using NUnit.Framework;
+using TestTask_GZipArchiver.Core.Models;
+using TestTask_GZipArchiver.Core.Services;
+
+namespace TestTask_GZipArchiver.Tests
+{
+    [TestFixture]
+    public class ArchivationServiceTests
+    {
+        private ArchivationService _sut;
+
+        [SetUp]
+        public void Init()
+        {
+            _sut = new ArchivationService();
+        }
+
+        [Test]
+        public void Should_compress_file()
+        {
+            var inputFile = new FileInfo("..\\..\\files\\file_to_compress.pdf");
+            var outputFile = new FileInfo("..\\..\\files\\file_to_compress.pdf.gz");
+
+            var args = new[]
+            {
+                "compress",
+                inputFile.FullName,
+                outputFile.FullName
+            };
+
+            RunningArguments.Bind(args);
+
+            _sut.CompressFile(RunningArguments.Current.InputPath, RunningArguments.Current.OutputPath);
+
+            Assert.That(outputFile.Exists);
+            Assert.That(outputFile.Length < inputFile.Length);
+        }
+
+        [Test]
+        public void Should_decompress_file()
+        {
+            var inputFile = new FileInfo("..\\..\\files\\file_to_decompress.pdf.gz");
+            var outputFile = new FileInfo("..\\..\\files\\file_to_decompress.pdf");
+
+            var args = new[]
+            {
+                "decompress",
+                inputFile.FullName,
+                outputFile.FullName
+            };
+
+            RunningArguments.Bind(args);
+
+            _sut.CompressFile(RunningArguments.Current.InputPath, RunningArguments.Current.OutputPath);
+
+            Assert.That(outputFile.Exists);
+            Assert.That(outputFile.Length < inputFile.Length);
+        }
+    }
+}
