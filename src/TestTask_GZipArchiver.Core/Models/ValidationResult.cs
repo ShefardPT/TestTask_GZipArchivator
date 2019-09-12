e109e0acb916e08985125dbb1d5d51f0cc74664b
Shefer Alexander <shefard55r@gmail.com>
2019-09-13 05:24:38 +0600

    - ValidationResult class and ValidationService were added, - Method to check if specified file is .gz archive was implemented,

---
+++ src/TestTask_GZipArchiver.Core/Models/ValidationResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestTask_GZipArchiver.Core.Models
+{
+    public class ValidationResult
+    {
+        public ValidationResult()
+        {
+            
+        }
+
+        public ValidationResult(bool isValid)
+        {
+            IsValid = isValid;
+        }
+
+        public ValidationResult(bool isValid, string message) : this(isValid)
+        {
+            Message = message;
+        }
+
+        public bool IsValid { get; set; }
+        public string Message { get; set; }
+    }
+}
