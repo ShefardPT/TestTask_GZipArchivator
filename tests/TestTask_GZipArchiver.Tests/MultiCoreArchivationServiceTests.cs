63123dd0580035ffd7859a59965280e16a03e499
Shefer Alexander <shefard55r@gmail.com>
2019-09-22 16:36:10 +0600

    - Test were modified

--- tests/TestTask_GZipArchiver.Tests/MultiCoreArchivationServiceTests.cs
+++ tests/TestTask_GZipArchiver.Tests/MultiCoreArchivationServiceTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
 using NUnit.Framework;
 using TestTask_GZipArchiver.Core.Models;
 using TestTask_GZipArchiver.Core.Services;
@@ -13,23 +14,31 @@ namespace TestTask_GZipArchiver.Tests
     {
         private MultiCoreArchivationService _sut;
 
+        private DirectoryInfo _testFilesDir;
+        private FileInfo _fileToCompress;
+        private FileInfo _fileToDecompress;
+
         [SetUp]
         public void Init()
         {
             _sut = new MultiCoreArchivationService();
+            _testFilesDir = new DirectoryInfo("..\\..\\..\\files");
+
+            if (!_testFilesDir.Exists)
+            {
+                _testFilesDir.Create();
+            }
+
+            _fileToCompress = new FileInfo(_testFilesDir.FullName + "\\file_to_compress.pdf");
+            _fileToDecompress = new FileInfo(_testFilesDir.FullName + "\\file_to_decompress.pdf.gz");
         }
 
         [Test]
         public void Should_compress_file()
         {
-            var inputFile = new FileInfo("..\\..\\..\\files\\file_to_compress.pdf");
-            var outputFile = new FileInfo("..\\..\\..\\files\\file_to_compress.pdf.gz");
-
-            if (outputFile.Exists)
-            {
-                outputFile.Delete();
-            }
-
+            var inputFile = _fileToCompress;
+            var outputFile = new FileInfo(inputFile.FullName + ".gz");
+            
             var args = new[]
             {
                 "compress",
@@ -41,23 +50,23 @@ namespace TestTask_GZipArchiver.Tests
 
             _sut.CompressFile(RunningArguments.Current.InputPath, RunningArguments.Current.OutputPath);
 
-            outputFile = new FileInfo("..\\..\\..\\files\\file_to_compress.pdf.gz");
+            outputFile = new FileInfo(outputFile.FullName);
 
             Assert.That(outputFile.Exists);
             Assert.That(outputFile.Length < inputFile.Length);
+
+            if (outputFile.Exists)
+            {
+                outputFile.Delete(); 
+            }
         }
 
         [Test]
         public void Should_decompress_file()
         {
-            var inputFile = new FileInfo("..\\..\\..\\files\\file_to_decompress.pdf.gz");
-            var outputFile = new FileInfo("..\\..\\..\\files\\file_to_decompress.pdf");
-
-            if (outputFile.Exists)
-            {
-                outputFile.Delete();
-            }
-
+            var inputFile = _fileToDecompress;
+            var outputFile = new FileInfo(inputFile.FullName.Remove(inputFile.FullName.Length - 3));
+            
             var args = new[]
             {
                 "decompress",
@@ -69,10 +78,15 @@ namespace TestTask_GZipArchiver.Tests
 
             _sut.DecompressFile(RunningArguments.Current.InputPath, RunningArguments.Current.OutputPath);
 
-            outputFile = new FileInfo("..\\..\\..\\files\\file_to_decompress.pdf");
+            outputFile = new FileInfo(outputFile.FullName);
 
             Assert.That(outputFile.Exists);
             Assert.That(outputFile.Length > inputFile.Length);
+
+            if (outputFile.Exists)
+            {
+                outputFile.Delete();
+            }
         }
     }
 }
